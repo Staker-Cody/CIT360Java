4fc6be0beb1d67985425696be1f94fb6488470b4
Staker-Cody <cstaker28@gmail.com>
2017-12-07 18:03:33 -0800

    adding hibernate folder

---
+++ hibernate/src/com/company/hibernate/Employee.java
@@ -0,0 +1,60 @@
+package com.company.hibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee{
+    @Id
+    @Column(name="id")
+    private int id;
+
+    @Column(name="ename")
+    private String ename;
+
+
+    @Column(name ="mobile")
+    private int mobile;
+
+    public Employee(int id, String ename, int mobile) {
+        this.id = id;
+        this.ename = ename;
+        this.mobile = mobile;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public int getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(int mobile) {
+        this.mobile = mobile;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", ename='" + ename + '\'' +
+                ", mobile=" + mobile +
+                '}';
+    }
+}
